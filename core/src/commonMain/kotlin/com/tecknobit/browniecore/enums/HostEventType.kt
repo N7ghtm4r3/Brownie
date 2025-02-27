7ba7f67a41386f883ff792e7a05b1bd87d8f2d9e
N7ghtm4r3 <maurizio.manuel2003@gmail.com>
2025-02-27 11:41:34 +0100

    Added the HostEventType

---
+++ core/src/commonMain/kotlin/com/tecknobit/browniecore/enums/HostEventType.kt
@@ -0,0 +1,19 @@
+package com.tecknobit.browniecore.enums
+
+enum class HostEventType {
+
+    ONLINE,
+
+    OFFLINE,
+
+    REBOOTING,
+
+    RESTARTED,
+
+    SERVICE_ADDED,
+
+    SERVICE_REMOVED,
+
+    SERVICE_DELETED
+
+}
