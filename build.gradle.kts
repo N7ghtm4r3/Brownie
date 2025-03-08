0f298966a9621c0252179881309f80fdb5101dcf
N7ghtm4r3 <maurizio.manuel2003@gmail.com>
2025-03-08 14:53:10 +0100

    Generated the Dokka's docu!

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,9 @@
+import org.jetbrains.dokka.DokkaConfiguration.Visibility.*
+import org.jetbrains.dokka.base.DokkaBase
+import org.jetbrains.dokka.base.DokkaBaseConfiguration
+import org.jetbrains.dokka.gradle.DokkaMultiModuleTask
+import org.jetbrains.dokka.gradle.DokkaTaskPartial
+
 plugins {
     kotlin("jvm")
     id("org.jetbrains.dokka") version "2.0.0"
@@ -10,7 +16,7 @@ buildscript {
     }
 }
 
-/*subprojects {
+subprojects {
     apply(plugin = "org.jetbrains.dokka")
     tasks.withType<DokkaTaskPartial>().configureEach {
         dokkaSourceSets.configureEach {
@@ -18,16 +24,16 @@ buildscript {
             documentedVisibilities.set(setOf(PUBLIC, PROTECTED, PRIVATE))
         }
     }
-}*/
+}
 
 repositories {
     mavenCentral()
 }
-/*
+
 tasks.withType<DokkaMultiModuleTask> {
     outputDirectory.set(layout.projectDirectory.dir("docs"))
     pluginConfiguration<DokkaBase, DokkaBaseConfiguration> {
         customAssets = listOf(file("docs/logo-icon.svg"))
         footerMessage = "(c) 2025 Tecknobit"
     }
-}*/
+}
