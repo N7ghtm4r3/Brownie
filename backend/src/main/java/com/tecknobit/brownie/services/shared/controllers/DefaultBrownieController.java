ffe67d52404e15499814f427fdfef4b4196ff2a5
N7ghtm4r3 <maurizio.manuel2003@gmail.com>
2025-03-08 10:12:40 +0100

    Working on :backend documentation, documented the shared folder

--- backend/src/main/java/com/tecknobit/brownie/services/shared/controllers/DefaultBrownieController.java
+++ backend/src/main/java/com/tecknobit/brownie/services/shared/controllers/DefaultBrownieController.java
@@ -5,6 +5,7 @@ import com.tecknobit.brownie.services.hosts.services.HostsService;
 import com.tecknobit.brownie.services.session.entity.BrownieSession;
 import com.tecknobit.brownie.services.session.service.BrownieSessionsService;
 import com.tecknobit.equinoxbackend.environment.services.DefaultEquinoxController;
+import com.tecknobit.equinoxbackend.environment.services.builtin.controller.EquinoxController;
 import com.tecknobit.equinoxcore.annotations.Assembler;
 import com.tecknobit.equinoxcore.annotations.FutureEquinoxApi;
 import com.tecknobit.equinoxcore.annotations.Returner;
@@ -24,14 +25,30 @@ import static com.tecknobit.equinoxcore.network.Requester.RESPONSE_STATUS_KEY;
 import static com.tecknobit.equinoxcore.network.ResponseStatus.FAILED;
 import static com.tecknobit.equinoxcore.network.ResponseStatus.SUCCESSFUL;
 
+/**
+ * The {@code DefaultBrownieController} class is useful to give the base behavior of the <b>Brownie's controllers</b>
+ *
+ * @author N7ghtm4r3 - Tecknobit
+ * @see EquinoxController
+ * @see DefaultEquinoxController
+ */
 @RestController
 public class DefaultBrownieController extends DefaultEquinoxController {
 
+    /**
+     * {@code SOMETHING_WENT_WRONG_MESSAGE} the key of the message sent to the clients when a remote SSH connection failed
+     */
     public static final String SOMETHING_WENT_WRONG_MESSAGE = "something_went_wrong_key";
 
+    /**
+     * {@code sessionsService} the support service used to manage the sessions data
+     */
     @Autowired
     protected BrownieSessionsService sessionsService;
 
+    /**
+     * {@code hostsService} the support service used to manage the hosts data
+     */
     @Autowired
     protected HostsService hostsService;
 
@@ -42,13 +59,30 @@ public class DefaultBrownieController extends DefaultEquinoxController {
     )
     protected MessageSource messageSource;
 
+    /**
+     * {@code currentBrownieSession} the current Brownie's session used in the request
+     */
     protected BrownieSession currentBrownieSession;
 
+    /**
+     * Method to check whether a session exists by its id
+     *
+     * @param sessionId The identifier of the session to check
+     * @return whether the session exists as {@code boolean}
+     */
     protected boolean sessionExists(String sessionId) {
         currentBrownieSession = sessionsService.getBrownieSession(sessionId);
         return currentBrownieSession != null;
     }
 
+    /**
+     * Method to retrieve a {@link BrownieHost} checking the identifier of the session and its identifier
+     *
+     * @param sessionId The identifier of the session owner of the host
+     * @param hostId The identifier of the host to retrieve
+     *
+     * @return if the host owned by the session and exists as {@link BrownieHost}, null otherwise
+     */
     protected BrownieHost getBrownieHostIfAllowed(String sessionId, String hostId) {
         return hostsService.getBrownieHost(sessionId, hostId);
     }
--- backend/src/main/java/com/tecknobit/brownie/services/shared/entities/BrownieEvent.java
+++ backend/src/main/java/com/tecknobit/brownie/services/shared/entities/BrownieEvent.java
@@ -8,20 +8,44 @@ import jakarta.persistence.MappedSuperclass;
 
 import static com.tecknobit.browniecore.ConstantsKt.EVENT_DATE_KEY;
 
+/**
+ * The {@code BrownieEvent} class is useful to represent an event related to a Brownie's items
+ *
+ * @author N7ghtm4r3 - Tecknobit
+ * @see EquinoxItem
+ */
 @Structure
 @MappedSuperclass
 public abstract class BrownieEvent extends EquinoxItem {
 
+    /**
+     * {@code eventDate} the date of the event when occurred
+     */
     @Column(name = EVENT_DATE_KEY)
     protected final long eventDate;
 
+    /**
+     * {@code extra} extra information related to the event
+     */
     @Column
     protected final String extra;
 
+    /**
+     * Constructor to instantiate the object
+     *
+     * @apiNote empty constructor required
+     */
     public BrownieEvent() {
         this(null, 0, null);
     }
 
+    /**
+     * Constructor to instantiate the object
+     *
+     * @param id        The identifier of the event
+     * @param eventDate The date of the event when occurred
+     * @param extra     The extra information related to the event
+     */
     public BrownieEvent(String id, long eventDate, Object extra) {
         super(id);
         this.eventDate = eventDate;
@@ -31,11 +55,21 @@ public abstract class BrownieEvent extends EquinoxItem {
         this.extra = extraValue;
     }
 
+    /**
+     * Method to get the {@link #eventDate} instance
+     *
+     * @return the {@link #eventDate} instance as {@code long}
+     */
     @JsonGetter(EVENT_DATE_KEY)
     public long getEventDate() {
         return eventDate;
     }
 
+    /**
+     * Method to get the {@link #extra} instance
+     *
+     * @return the {@link #extra} instance as {@link Object}
+     */
     public Object getExtra() {
         return extra;
     }
